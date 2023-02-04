8e38efc38969b6e9604d7bbf58f49f2cef875161
Kravchenko Yevhenii <111376447+iEfir@users.noreply.github.com>
2023-02-04 18:15:37 +0200

    task-01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let intervalId = setInterval(getRandomHexColor, 1000);
+
+btnStart.addEventListener('click', onBtnStart);
+btnStop.addEventListener('click', onBtnStop);
+
+function updateColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onBtnStart() {
+  intervalId = setInterval(updateColor, 1000);
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+}
+
+function onBtnStop() {
+  clearInterval(intervalId);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+}
