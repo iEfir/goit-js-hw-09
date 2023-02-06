a86a75c977362d04d87cf050f44c68f71418d530
Kravchenko Yevhenii <111376447+iEfir@users.noreply.github.com>
2023-02-06 11:01:35 +0200

    task-02 add Notify

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix';
 
 const daysEl = document.querySelector('span[data-days]');
 const hoursEl = document.querySelector('span[data-hours]');
@@ -17,34 +18,38 @@ const options = {
   onClose(selectedDates) {
     console.log(selectedDates[0]);
     if (selectedDates[0] < new Date()) {
-      window.alert('Please choose a date in the future');
+      Notify.failure('Please choose a date in the future');
     }
     if (selectedDates[0] > new Date()) {
       btnStart.disabled = false;
-
-      let interavlId = setInterval(countDownDate(), 1000);
+      btnStart.addEventListener('click', onBtnStart);
+      let interavlId = null;
 
       function onBtnStart() {
+        btnStart.disabled = true;
         interavlId = setInterval(countDownDate, 1000);
       }
-      btnStart.addEventListener('click', onBtnStart);
 
       function countDownDate(ms) {
         ms = selectedDates[0] - new Date();
-        const second = 1000;
-        const minute = second * 60;
-        const hour = minute * 60;
-        const day = hour * 24;
+        if (ms === 0) {
+          clearInterval(interavlId);
+        } else {
+          const second = 1000;
+          const minute = second * 60;
+          const hour = minute * 60;
+          const day = hour * 24;
 
-        const days = Math.floor(ms / day);
-        const hours = Math.floor((ms % day) / hour);
-        const minutes = Math.floor(((ms % day) % hour) / minute);
-        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+          const days = Math.floor(ms / day);
+          const hours = Math.floor((ms % day) / hour);
+          const minutes = Math.floor(((ms % day) % hour) / minute);
+          const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-        daysEl.textContent = addZero(days);
-        hoursEl.textContent = addZero(hours);
-        minutesEl.textContent = addZero(minutes);
-        secondsEl.textContent = addZero(seconds);
+          daysEl.textContent = addZero(days);
+          hoursEl.textContent = addZero(hours);
+          minutesEl.textContent = addZero(minutes);
+          secondsEl.textContent = addZero(seconds);
+        }
       }
     }
   },
